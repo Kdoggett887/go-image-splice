33ce25ff0b6d1558486e7dbaf1298c0c8d5de97f
Keith Doggett <keith.doggett887@gmail.com>
2019-11-14 17:53:43 -0500

    added ability to splice gif onto an image

--- main/main.go
+++ main/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	splice "go-image-splice"
+	"image/gif"
 	"image/jpeg"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 4 {
 		println("not enough args")
 		os.Exit(1)
 	}
@@ -15,42 +16,70 @@ func main() {
 	source, err := os.Open(sourcePath)
 	if err != nil {
 		println("Error opening scope")
-		panic("bah")
+		os.Exit(1)
 	}
 
 	sourceImg, err := jpeg.Decode(source)
 	if err != nil {
 		println("could not decode source img")
 	}
+	defer source.Close()
 
 	targetPath := os.Args[2]
 	target, err := os.Open(targetPath)
 	if err != nil {
-		println("Error opening drinks")
-		panic("bah")
+		println("Error opening target img")
+		os.Exit(1)
 	}
 
 	targetImg, err := jpeg.Decode(target)
 	if err != nil {
 		println("could not decode target img")
 	}
+	defer target.Close()
 
 	bounds := [4][2]int{
-		{0, 400},
+		{0, 800},
 		{0, 0},
-		{600, 400},
-		{600, 0},
+		{1200, 800},
+		{1200, 0},
 	}
 	t := splice.NewTarget(&targetImg, &bounds)
 	s := &splice.Source{Img: &sourceImg}
 
-	newImg := splice.SpliceImgs(s, t)
+	newImg := splice.Imgs(s, t)
 
 	output, err := os.Create("result.jpg")
 	if err != nil {
 		println("couldn't make file")
 	}
+	defer output.Close()
 
-	jpeg.Encode(output, newImg, &jpeg.Options{jpeg.DefaultQuality})
+	jpeg.Encode(output, newImg, &jpeg.Options{Quality: jpeg.DefaultQuality})
 
+	gifPath := os.Args[3]
+	source2, err := os.Open(gifPath)
+	if err != nil {
+		println("Error opening source gif")
+		os.Exit(1)
+	}
+
+	sourceGif, err := gif.DecodeAll(source2)
+	if err != nil {
+		println("could not decode source gif")
+	}
+	defer source2.Close()
+
+	newGif := splice.GifToImg(sourceGif, t)
+
+	output2, err := os.Create("gif-result.gif")
+	if err != nil {
+		println("couldn't make gif")
+	}
+	defer output2.Close()
+
+	err = gif.EncodeAll(output2, newGif)
+	if err != nil {
+		println(err)
+	}
 }
